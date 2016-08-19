89d72ee25e14f56f8ca8136217a4ee3ba40285d3
ywm <156859885@qq.com>
2016-08-19 13:55:48 +0800

    测试修改

--- src/com/ywm/app/HelloGit.java
+++ src/com/ywm/app/HelloGit.java
@@ -3,7 +3,7 @@ package com.ywm.app;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git!!");
 		System.out.println("eclipse push test");
 	}
 
